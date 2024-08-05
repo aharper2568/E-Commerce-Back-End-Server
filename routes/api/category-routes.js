56f43c68bd7771c2fd7613dbb209b38ae11dc92a
aharper2568 <anthonyharper568@gmail.com>
2024-08-05 15:05:06 -0400

    removed redudant delete route from category-routes, was typo and duplicate update route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -65,25 +65,6 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', async (req, res) => {
-  // delete a category by its `id` value
-  try {
-    const category = await Category.update(req.body, {
-      where: {
-        id: req.params.id,
-      },
-    });
-
-    if (!category[0]) {
-      res.status(404).json({ message: 'No category found with this id!' });
-      return;
-    }
-
-    res.status(200).json(category);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
 
 router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
